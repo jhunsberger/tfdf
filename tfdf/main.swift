0fc94d452be7003778ad650f083ebf156514eaa6
jeffgravitywell <jeff@gravitywellgroup.com>
2014-06-05 16:11:52 -0400

    Initial Commit

---
+++ tfdf/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  tfdf
+//
+//  Created by Jeff Hunsberger on 6/5/14.
+//  Copyright (c) 2014 Gravity Well Apps. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
